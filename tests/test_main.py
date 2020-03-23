ae6f88bded4c3a7be7a30d8537d028ac9bdac897
Simon Szalai <simon.szalai@gmail.com>
2020-03-23 16:37:29 +0100

    TEST: fixed main test

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,35 +13,40 @@ class TestMain:
         # Create temporary directory for testing
         cls.tmp_path = os.path.join(os.path.abspath(os.path.join(os.path.abspath(__file__), "../../images")), "test_tmp")
         cls.db_name = "sorterbot_test"
+        cls.table_name = "test_table"
 
         cls.main = Main(db_name=cls.db_name, base_img_path=cls.tmp_path)
 
     @pytest.mark.parametrize("image_name", ["valid_image.jpg", "corrupted_image.jpg", "missing_image.jpg"])
-    def test_download_image(self, image_name):
-        self.main.process_image("sorterbot_test_bucket", image_name)
+    def test_process_image(self, image_name):
+        self.main.process_image(session_id="sorterbot_test_bucket", image_name=image_name)
 
     def test_vectorize_session_images(self):
         pairings = self.main.vectorize_session_images()
-        print(pairings)
 
         shutil.rmtree(self.tmp_path)
 
-        # Close connection to test database
-        self.main.postgres.close()
+        def get_clusters(items):
+            cluster_1 = []
+            cluster_2 = []
 
-        # Open another connection to maintenenace database so test database can be dropped
-        self.main.postgres = Postgres(db_name="postgres")
-        self.main.postgres.open()
-        self.main.postgres.cursor.execute(f"DROP DATABASE IF EXISTS {self.db_name};")
-        self.main.postgres.close()
+            for item in items:
+                if item["cluster"] == 0:
+                    cluster_1.append(item["filename"])
+                elif item["cluster"] == 1:
+                    cluster_2.append(item["filename"])
 
-        assert pairings == expected_main_results
+            return cluster_1, cluster_2
+
+        pairings_1, pairings_2 = get_clusters(pairings)
+        expected_1, expected_2 = get_clusters(expected_main_results)
+
+        # Order of clusters in the list is not consistent (as returned by K-Means algorithm) so both cases below should pass test
+        # (The same elements are consistently in the same cluster, but sometimes cluster 0 is the previous run's cluster 1)
+        assert (pairings_1 == expected_1 and pairings_2 == expected_2) or (pairings_2 == expected_1 and pairings_1 == expected_2)
 
     @classmethod
     def teardown_class(cls):
-        # Drop table to prevent subsequent test failures
-        cls.main.postgres.cursor.execute(f"DROP TABLE IF EXISTS {cls.table_name};")
-
         # Close connection to test database
         cls.main.postgres.close()
 
